d4c691d61209aa967a52f57d1715d1135d5f3613
Disaster <100930333+heuku@users.noreply.github.com>
2022-06-27 15:59:27 +0900

    Create AppConfig.java

---
+++ hello.core/AppConfig.java
@@ -0,0 +1,19 @@
+public class AppConfig {
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+//        return new FixDiscountPolicy();
+    }
+}
